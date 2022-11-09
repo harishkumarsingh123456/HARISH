1c95d9dbe81df15fe2e68e1153ad27bcfa5a7e6b
Harish <harishkumarsingh999@gmail.com>
2022-11-09 20:24:07 +0530

    my first commit

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Style.css"
+ let Mohan = ()=>{
+document.write(sonu)
+ }
+
+const Home1 = () => {
+  return (
+    <div className='ram'>
+        hi i am harish{Mohan}
+    </div>
+  )
+}
+
+export default Home1
